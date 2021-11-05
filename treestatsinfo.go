49b73fd21e318676834a65b921174e9d691c2761
ppenguin <hieronymusv@gmail.com>
2021-11-05 14:11:21 +0100

    fix behaviour when dbase not found

--- treestatsinfo.go
+++ treestatsinfo.go
@@ -1,6 +1,7 @@
 package filetypestats
 
 import (
+	"fmt"
 	"path/filepath"
 
 	"github.com/ppenguin/filetypestats/ftsdb"
@@ -16,28 +17,38 @@ type TreeStatsInfo struct {
 
 // NewTreeStatsInfo instantiates a TreeStatsInfo, a convenience object providing query functions for a file info DB
 func NewTreeStatsInfo(filedb string) (*TreeStatsInfo, error) {
-	var fdb *ftsdb.FileTypeStatsDB
-	var err error
-
-	if fdb, err = ftsdb.NewNoOpen(filedb); err != nil {
-		return nil, err
-	}
+	fdb, err := ftsdb.NewNoOpen(filedb)
 	return &TreeStatsInfo{
 		dbFile: filedb,
 		ftsDB:  fdb,
-	}, nil
+	}, err // we always return an object, because the open call for the DB is basically just a test, if no DB it can come into existence later
 }
 
 // FTStatsDirs returns the FileTypeStats per dir
 // call with dir="/my/dir/*" to get the recursive totals under that dir, or set recursive=true
 func (tsi *TreeStatsInfo) FTStatsDirs(dirs []string, recursive bool) (types.FileTypeDirStats, error) {
-	tsi.ftsDB.Open()
+	if err := tsi.openDB(); err != nil {
+		return types.FileTypeDirStats{}, err
+	}
 	if recursive {
 		for i, d := range dirs {
 			dirs[i] = filepath.Join(d, "*")
 		}
 	}
 	res, err := tsi.ftsDB.FTStatsDirs(dirs)
-	tsi.ftsDB.Close()
+	tsi.closeDB()
 	return res, err
+}
+
+func (tsi *TreeStatsInfo) openDB() error {
+	if tsi.ftsDB != nil {
+		tsi.ftsDB.Open()
+	}
+	return fmt.Errorf("TreeStatsInfo: no DB assigned, no file info available")
+}
+
+func (tsi *TreeStatsInfo) closeDB() {
+	if tsi.ftsDB != nil {
+		tsi.ftsDB.Close()
+	}
 }
