a7a2496cea174a0b4f4e3ec90be2b2cf13f6f39f
ppenguin <hieronymusv@gmail.com>
2021-11-16 21:50:02 +0100

    adapt test to refactor

--- test/main.go
+++ test/main.go
@@ -3,10 +3,19 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/ppenguin/filetypestats"
-	utils "github.com/ppenguin/gogenutils"
+	"github.com/ppenguin/filetypestats/treestatsquery"
+	"github.com/ppenguin/filetypestats/utils"
+	"github.com/ppenguin/gogenutils"
+)
+
+var (
+	testdirs = []string{"Documents", "Downloads"} // just Q&D, make sure to put existing dirs here
+	dbfile   string
+	tsw      *filetypestats.TreeStatsWatcher
 )
 
 func exitErr(err error) {
@@ -15,39 +24,137 @@ func exitErr(err error) {
 }
 
 func main() {
-	dirs := []string{"/usr/share"}
 
-	ts := time.Now()
-	ShowFileTypeStats(dirs)
-	fmt.Printf("\nFileTypeStats took %v\n", time.Since(ts))
+	dirs := getTestDirs()
 
-	ts = time.Now()
-	ShowFileSizeCount(dirs)
-	fmt.Printf("\nFileSizeCount took %v\n", time.Since(ts))
+	wd, err := os.Getwd()
+	if err != nil {
+		exitErr(err)
+	}
+
+	dbfile = filepath.Join(wd, "testdb.sqlite")
+	tsw, err = filetypestats.NewTreeStatsWatcher(dirs, dbfile)
+	if err != nil {
+		exitErr(err)
+	}
+
+	// do some basic checks...
+
+	// 1. We just initialised with dirs, that means normally a scan will be running for them...
+	if err := checkInitialScan(tsw); err != nil {
+		exitErr(err)
+	}
+
+	// 2. check selected files against query results
+	// this is only approximate, because the selection criteria for the file-scan and the DB query are a bit different, and recursiveness also
+	if err := checkFilesGlob("image", []string{"*.jpg", "*.png"}); err != nil {
+		exitErr(err)
+	}
+
+	if err := checkFilesGlob("application", []string{"*.pdf", "*.*z*", "*.exe", "*.txt"}); err != nil {
+		exitErr(err)
+	}
+
+	fmt.Println("Starting to watch, press ctrl+c to exit...")
+	fmt.Printf("Manipulate the contents of %v to test inotify\n", tsw.Dirs())
+	tsw.WatchAll()
+	fmt.Println("All watchers finished")
 }
 
-func ShowFileTypeStats(dirs []string) {
-	var (
-		totCount uint   = 0
-		totSize  uint64 = 0
-	)
-	if ftStats, err := filetypestats.WalkFileTypeStats(dirs); err != nil {
-		exitErr(err)
-	} else {
-		fmt.Println("Test with WalkFileTypeStats:")
-		for k, v := range ftStats {
-			fmt.Printf("%d %s files taking %s of space\n", v.FileCount, k, utils.ByteCountSI(v.NumBytes))
-			totCount += v.FileCount
-			totSize += v.NumBytes
+func getTestDirs() []string {
+	dirs := make([]string, 0)
+	hdir, err := os.UserHomeDir()
+	if err != nil {
+		return dirs
+	}
+	for _, d := range testdirs {
+		if fi, err := os.Lstat(filepath.Join(hdir, d)); err == nil && fi.IsDir() {
+			dirs = append(dirs, filepath.Join(hdir, d))
 		}
-		fmt.Printf("\nTotal %d files taking %s of space\n", totCount, utils.ByteCountSI(totSize))
 	}
+	return dirs
 }
 
-func ShowFileSizeCount(dirs []string) {
-	if fStats, err := filetypestats.WalkFileSizeCount(dirs); err != nil {
-		exitErr(err)
-	} else {
-		fmt.Printf("Test with WalkFileSizeCount:\n%d files taking %s of space\n", fStats.FileCount, utils.ByteCountSI(fStats.NumBytes))
+func checkInitialScan(tsw *filetypestats.TreeStatsWatcher) error {
+	errs := gogenutils.NewErrors()
+	// ds := tsw.Dirs()
+	anyscan := true
+	for anyscan {
+		anyscan = false
+		for _, d := range tsw.Dirs() {
+			stats, err := getStatSummaryStr(d)
+			errs.AddIf(err)
+			if tsw.ScanRunning(d) {
+				fmt.Printf("scan for %s running, current stats: \n%s\n", d, stats)
+				anyscan = true
+			} else {
+				anyscan = anyscan || false
+				fmt.Printf("scan for %s finished (anyscan: %t), current stats: \n%s\n", d, anyscan, stats)
+			}
+		}
+		time.Sleep(1 * time.Second)
 	}
+	return errs.Err()
+}
+
+func getStatSummaryStr(dir string) (string, error) {
+	dirs := []string{utils.DirStar(dir)}
+	stats, err := treestatsquery.FTStatsSum(dbfile, dirs)
+	if err != nil {
+		return "", err
+	}
+	return stats.ToString(), nil
+}
+
+func checkFilesGlob(ftype string, globs []string) error {
+	// find some files and check their data against specific query data
+	files := []string{}
+	for _, d := range tsw.Dirs() {
+		for _, g := range globs {
+			files = append(files, getRelGlob(d, g)...)
+		}
+	}
+	return checkFTStats(files, ftype)
+}
+
+func checkFTStats(files []string, ftype string) error {
+	// find some files and check their data against specific query data
+	if len(files) > 0 {
+		ts, tc := getFileSizeCount(files)
+		fmt.Printf("\nFound the following %s files: %v\ntotal size: %6s\ttotal count: %5d\n", ftype, files, gogenutils.ByteCountSI(ts), tc)
+		qd := utils.StringSliceApply(tsw.Dirs(), utils.DirTrailSep)
+		stats, err := treestatsquery.FTStatsSum(dbfile, qd) // now we basically expect that the images fount in the top-level dir correspond to those in the query result
+		if err != nil {
+			fmt.Printf("ERROR performing query: %s", err.Error())
+			return err
+		}
+		if fst, ok := stats[ftype]; ok {
+			fmt.Printf("\nQuery result for %s files: \ntotal size: %6s\ttotal count: %5d\n", ftype, gogenutils.ByteCountSI(fst.NumBytes), fst.FileCount)
+		}
+	}
+	return nil
+}
+
+func getRelGlob(dir, pat string) []string {
+	fs, err := filepath.Glob(filepath.Join(dir, pat))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: %s", err.Error())
+		return []string{}
+	}
+	return fs
+
+}
+
+func getFileSizeCount(files []string) (uint64, uint) {
+	var size uint64 = 0
+	var count uint = 0
+	for _, f := range files {
+		if fi, err := os.Lstat(f); err == nil {
+			if !fi.IsDir() {
+				size += uint64(fi.Size())
+				count += 1
+			}
+		}
+	}
+	return size, count
 }
